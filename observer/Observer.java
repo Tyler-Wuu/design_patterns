41df7c56872ee83aa96543b01e96d398ae9d6aae
Tyler Wu <1079467460am@gmail.com>
2026-01-23 10:40:02 -0500

    Add observer fold and classes

---
+++ observer/Observer.java
@@ -0,0 +1,22 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author Tyler Wu
+ */
+public abstract class Observer {
+
+    protected Random rand;
+
+    public Observer(ArrayList<Baby> babies){
+        this.rand = new Random();
+        for(Baby baby : babies){
+            baby.registerObserver(this);
+        }
+    }
+
+    public abstract void update(Cry cry, Baby baby);
+    
+}
