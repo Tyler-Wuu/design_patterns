a9d4fa9b9a593d69904cec7f52a4c93d38fdc00d
Tyler Wu <1079467460am@gmail.com>
2026-02-11 21:26:18 -0500

    Finish editing Javadoc

--- singleton/OutfitCreator.java
+++ singleton/OutfitCreator.java
@@ -21,14 +21,25 @@ public class OutfitCreator {
         this.rand = new Random();
     }
 
+    /**
+     * static OutfitCreator method
+     * please note this method is not responsible for creating the instance
+     * @return the only instance of the OutfitCreator
+     */
     public static OutfitCreator getInstance() {
         return outfitCreator;
     }
 
+    /**
+     * This method will return a radomly choosen cloth from the txt file in string
+     * @param season the season that the cloth must contain
+     * @return a randomly choosen cloth from the txt files, in the format of:
+     * Season: [list of season] Cloth: "cloth name" Color: "cloth color"
+     */
     public String getOutfit(Season season) {
-        while (true) {
+        while (true) {  // keep looping
             ClothingPart clothingPartChoice = null;
-            switch (this.rand.nextInt(2)) {
+            switch (this.rand.nextInt(2)) {  // choose a piece of cloth part(0-2)
                 case 0:
                     clothingPartChoice = ClothingPart.TOP;
                     break;
@@ -43,11 +54,16 @@ public class OutfitCreator {
                     break;
             }
 
-            int clothChoice = this.rand.nextInt(clothing.get(clothingPartChoice).size());
-            if (clothing.get(clothingPartChoice).get(clothChoice).hasSeason(season)) {
-                return clothing.get(clothingPartChoice).get(clothChoice).toString();
+            int clothChoiceIndex = 
+            this.rand.nextInt(clothing.get(clothingPartChoice).size());
+            // choose from index between 0-the size of the list
+            ClothingItem clothChoice = clothing.get(clothingPartChoice).get(clothChoiceIndex);
+
+            if (clothChoice.hasSeason(season)) {  // if this cloth fit for the season
+                return clothChoice.toString();
             }
+            // if the cloth did not fit the season this will keep loop
         }
     }
-    
+
 }
